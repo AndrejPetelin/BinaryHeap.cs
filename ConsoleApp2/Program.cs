70f41e4d3da4d4b52fbe355af36733755deab45e
Andrej Petelin <petelin.andrej@amis.net>
2018-02-15 19:36:49 +0100

    Added test for user-defined data objects

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using DataStructures;
+
+namespace ConsoleApp2
+{
+    struct UserData: IComparable
+    {
+        public UserData(string n, int s) { name = n; score = s; }
+        public string name;
+        public int score;
+
+        public int CompareTo(object obj)
+        {
+            UserData? ud = obj as UserData?;
+            return score.CompareTo(ud?.score);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryHeap<UserData> pqueue = new BinaryHeap<UserData>();
+
+            for (int i = 0; i < 30; ++i)
+            {
+                Random r = new Random();
+                pqueue.Push(new UserData("abc", r.Next()));
+
+            }
+
+
+            while (!pqueue.IsEmpty())
+            {
+                UserData user = pqueue.Pop();
+
+                Console.WriteLine(user.name + " " + user.score);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
